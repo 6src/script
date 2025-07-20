9b29396c13775ade2d35d705bbf186a4d5c19567
6src <sixsrc@gmail.com>
2025-07-20 10:43:18 +0200

    Update 6srcHub.lua

--- 6srcHub.lua
+++ 6srcHub.lua
@@ -1,17 +1,24 @@
 local scripts = {
-    [6678600773] = "https://github.com/6src/script/blob/main/RH2.lua", -- RH2 Mycourt
-    [7235817949] = "https://github.com/6src/script/blob/main/RH2.lua", -- RH2 Arcade
-    [8026790371] = "https://github.com/6src/script/blob/main/RH2.lua", -- RH2 Park
-    [14269621394] = "https://github.com/6src/script/blob/main/RH2.lua", -- RH2 Rec
-    [13272886093] = "https://github.com/6src/script/blob/main/RH2.lua", -- RH2 Plaza
-    [6585022145] = "https://github.com/6src/script/blob/main/RH2.lua", -- Build Creator
-    [16347334090] = "https://github.com/6src/script/blob/main/HoopNation2.lua", -- Hoop Nation 2 Park
-    [16110530195] = "https://github.com/6src/script/blob/main/HoopNation2.lua", -- Hoop Nation 2 Matchmaking
-    [16128274392] = "https://github.com/6src/script/blob/main/HoopNation2.lua", -- Hoop Nation 2 Mycourt 
-    [14259168147] = "https://github.com/6src/script/blob/main/BasketballLegends.lua", -- Basketball Legends 5v5
-    [14386691987] = "https://github.com/6src/script/blob/main/BasketballLegends.lua", -- Basketball Legends Park
-    [17652855195] = "hhttps://github.com/6src/script/blob/main/BasketballLegends.lua", -- Basketball Legends Rank
-    [134558434771720] = "https://github.com/6src/script/blob/main/ArcadeBasketball.lua", -- Basketball Arcade
+    -- RH2
+    [6678600773] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+    [7235817949] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+    [8026790371] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+    [14269621394] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+    [13272886093] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+    [6585022145] = "https://raw.githubusercontent.com/6src/script/main/RH2.lua",
+
+    -- Hoop Nation 2
+    [16347334090] = "https://raw.githubusercontent.com/6src/script/main/HoopNation2.lua",
+    [16110530195] = "https://raw.githubusercontent.com/6src/script/main/HoopNation2.lua",
+    [16128274392] = "https://raw.githubusercontent.com/6src/script/main/HoopNation2.lua",
+
+    -- Basketball Legends
+    [14259168147] = "https://raw.githubusercontent.com/6src/script/main/BasketballLegends.lua",
+    [14386691987] = "https://raw.githubusercontent.com/6src/script/main/BasketballLegends.lua",
+    [17652855195] = "https://raw.githubusercontent.com/6src/script/main/BasketballLegends.lua",
+
+    -- Arcade Basketball
+    [134558434771720] = "https://raw.githubusercontent.com/6src/script/main/ArcadeBasketball.lua",
 }
 
 local placeId = game.PlaceId
@@ -19,5 +26,5 @@ local placeId = game.PlaceId
 if scripts[placeId] then
     loadstring(game:HttpGet(scripts[placeId]))()
 else
-    print("No script available for this game!")
+    warn("No script found")
 end
