473fa26fe49c672fc860208cbb2ae22b74a65dff
6src <sixsrc@gmail.com>
2025-07-22 12:42:55 +0200

    Update 6srcHub.lua

--- 6srcHub.lua
+++ 6srcHub.lua
@@ -33,6 +33,11 @@ local scripts = {
     [11731575387] = "https://raw.githubusercontent.com/6src/script/main/HoopsLife.lua",
     [18894472013] = "https://raw.githubusercontent.com/6src/script/main/HoopsLife.lua",
     [17483792958] = "https://raw.githubusercontent.com/6src/script/main/HoopsLife.lua",
+
+    -- FF2
+
+    [8204899140] = "https://raw.githubusercontent.com/6src/script/main/FF2.lua",
+    [8206123457] = "https://raw.githubusercontent.com/6src/script/main/FF2.lua",
 }
 
 local placeId = game.PlaceId
